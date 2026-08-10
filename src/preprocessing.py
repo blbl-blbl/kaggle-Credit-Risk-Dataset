a5b0e19121f8a654b32362e6901015ff25cba0c5
Sergey <spect420@bk.ru>
2026-08-10 11:15:33 +0300

    small name fixes

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,30 +10,19 @@ from src.config import (NUMERIC_FEATURES,
 
 def build_preprocessor():
     # Numeric features preprocessing
-    numeric_pipeline_catboost = Pipeline(steps=[
+    numeric_pipeline = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='constant', fill_value=-1))
     ])
 
     # Categorial features preprocessing
-    categorial_pipeline_catboost = Pipeline(steps=[
+    categorial_pipeline = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
         ('zero_imputer', SimpleImputer(missing_values=0, strategy='constant', fill_value='missing'))
     ])
 
-    catboost_transformer = ColumnTransformer(transformers=[
-        ('numeric', numeric_pipeline_catboost, NUMERIC_FEATURES),
-        ('categorical', categorial_pipeline_catboost, CATEGORICAL_FEATURES)
+    transformer = ColumnTransformer(transformers=[
+        ('numeric', numeric_pipeline, NUMERIC_FEATURES),
+        ('categorical', categorial_pipeline, CATEGORICAL_FEATURES)
     ])
 
-    cat_features_indices = list(
-        range(
-            len(NUMERIC_FEATURES),
-            len(NUMERIC_FEATURES) + len(CATEGORICAL_FEATURES)
-        )
-    )
-
-    preprocessor_pipeline_catboost = Pipeline(steps=[
-        ('preprocessor', catboost_transformer)
-    ])
-
-    return preprocessor_pipeline_catboost
+    return transformer
